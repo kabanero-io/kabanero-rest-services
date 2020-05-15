0f2fdb907f93993616e308a9d3236ab88ae83502
davco01a <dacohen@us.ibm.com>
2020-05-14 23:15:49 -0400

    test

---
+++ pkg/cmd/service/my_first.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	x := [5]int{10, 20, 30, 40, 50}
+	var a [5]int 
+	
+	c :="First value" 
+	
+	a[2] = 7
+	
+	fmt.Println(a)
+	fmt.Println(x)
+	fmt.Println(c)
+	
+	c = "second value"
+	fmt.Println(c)
+	
+}
+
