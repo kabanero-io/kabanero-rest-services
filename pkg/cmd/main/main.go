b2175f6b4021aa03c2dd2e1bbec03468d4b56427
davco01a <dacohen@us.ibm.com>
2020-05-18 14:28:20 -0400

    fix

--- pkg/cmd/main/main.go
+++ pkg/cmd/main/main.go
@@ -3,8 +3,10 @@ package main
 import (
     "fmt"
     "log"
-}
+    "net/http"
+)
 
+// test rest endpoint
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome to the HomePage!")
     fmt.Println("Endpoint Hit: homePage")
