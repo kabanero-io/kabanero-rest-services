adaa37b6a87b2f949725b3940f6a7890d248cf5e
davco01a <dacohen@us.ibm.com>
2020-06-27 11:09:29 -0400

    working oauth proxy

---
+++ buildOfficial.sh
@@ -0,0 +1,8 @@
+#swagger generate server -A kabanero-rest-services -f swagger.yml
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build ./cmd/kabanero-rest-services-server/
+cp build/_output/bin/kabanero-rest-services/main bin/.
+cp build/_output/bin/kabanero-rest-services/main build/bin/.
+cp kabanero-rest-services-server build/bin/.
+docker rmi -f kabanero/kabanero-rest-services:0.11-rc.1
+docker build -f build/Dockerfile -t kabanero/kabanero-rest-services:0.11-rc.1 .
+docker push kabanero/kabanero-rest-services:0.11-rc.1
